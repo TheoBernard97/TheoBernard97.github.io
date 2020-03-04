f3458bcb9645a81d6baf1406060234957df8eef7
Théo Bernard <theo.bernard.94100@gmail.com>
2020-03-04 16:45:39 +0100

    JavaScript Refactoring

--- js/main.js
+++ js/main.js
@@ -1,47 +1,52 @@
-let modalTitleList = ["Le Blog de Zozor", "Tindog", "Roll a ball 3D"];
-let modalImageList = [
-  "images/zozor-modal.png",
-  "images/tindog-modal.png",
-  "images/roll-modal.png"
-];
-let modalTechList = ["HTML - CSS", "HTML - CSS - Bootstrap", "C# - Unity"];
-let modalDescriptionList = [
-  "Page d'un blog fictif, ce projet est aussi mon premier projet web.",
-  "Page de présentation fictive de l'application Tindog, ce projet est mon premier réalisé avec Bootstrap.",
-  "Jeu de plateforme 3D, ce projet de 2015 est mon tout premier. C'est à cette occasion que j'ai découvert les bases de la programmation."
-];
-let modalButtonLiveList = [
-  "https://theobernard97.github.io/Zozor",
-  "https://theobernard97.github.io/Tindog",
-  "https://theobernard97.itch.io/roll-our-earth"
-];
-let modalButtonList = [
-  "https://github.com/TheoBernard97/Zozor",
-  "https://github.com/TheoBernard97/Tindog",
-  "https://github.com/TheoBernard97/Roll-a-ball"
+const modal = [
+  {
+    id: 1,
+    title: "Le Blog de Zozor",
+    img: "images/zozor-modal.png",
+    tech: "HTML - CSS",
+    description:
+      "Page d'un blog fictif, ce projet est aussi mon premier projet web.",
+    live: "https://theobernard97.github.io/Zozor",
+    github: "https://github.com/TheoBernard97/Zozor"
+  },
+  {
+    id: 2,
+    title: "Tindog",
+    img: "images/tindog-modal.png",
+    tech: "HTML - CSS - Bootstrap",
+    description:
+      "Page de présentation fictive de l'application Tindog, ce projet est mon premier réalisé avec Bootstrap.",
+    live: "https://theobernard97.github.io/Tindog",
+    github: "https://github.com/TheoBernard97/Tindog"
+  },
+  {
+    id: 3,
+    title: "Roll a ball 3D",
+    img: "images/roll-modal.png",
+    tech: "C# - Unity",
+    description:
+      "Jeu de plateforme 3D, ce projet de 2015 est mon tout premier. C'est à cette occasion que j'ai découvert les bases de la programmation.",
+    live: "https://theobernard97.itch.io/roll-our-earth",
+    github: "https://github.com/TheoBernard97/Roll-a-ball"
+  }
 ];
 
 // Image preloader
 
-function preload(url) {
-  new Image().src = url;
-}
-
-for (let i = 0; i < modalImageList.length; i++) {
-  preload(modalImageList[i]);
-}
+modal.map(function(modal) {
+  new Image().src = modal.img;
+  console.log(modal.img);
+});
 
 // Change the content of the modal when the user click on "Learn more"
 
 function changeModalContent(x) {
-  document.querySelector("#exampleModalCenterTitle").innerHTML =
-    modalTitleList[x];
-  document.querySelector("#modal-image").src = modalImageList[x];
-  document.querySelector("#modal-tech-list").innerHTML = modalTechList[x];
-  document.querySelector("#modal-description").innerHTML =
-    modalDescriptionList[x];
-  document.querySelector("#modal-live-button").href = modalButtonLiveList[x];
-  document.querySelector("#modal-button").href = modalButtonList[x];
+  document.querySelector("#exampleModalCenterTitle").innerHTML = modal[x].title;
+  document.querySelector("#modal-image").src = modal[x].img;
+  document.querySelector("#modal-tech-list").innerHTML = modal[x].tech;
+  document.querySelector("#modal-description").innerHTML = modal[x].description;
+  document.querySelector("#modal-live-button").href = modal[x].live;
+  document.querySelector("#modal-button").href = modal[x].github;
 }
 
 document.querySelector("#zozor-button").addEventListener("click", function() {
